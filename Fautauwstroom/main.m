f0a7eab729eedef2aaacb661ad60b427e1ae1ff4
Matthias <Matthias@MacBook-Pro-van-Matthias-6.local>
2013-09-20 16:31:16 +0200

    Initial Commit

---
+++ Fautauwstroom/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Fautauwstroom
+//
+//  Created by Matthias on 20/09/13.
+//  Copyright (c) 2013 Matthias. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
